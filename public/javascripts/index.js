d0ff328da8c130f75cde7d8baa2a5201f38f76b1
Joel Kemp <mrjoelkemp@gmail.com>
2014-06-15 23:44:31 -0400

    Better styling

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,41 +1,5 @@
 ;(function (window, $) {
-
-  function loadLink() {
-
-  }
-
-  function checkForLink() {
-    // Look at the url params and pull out the website and top fields
-    // if they exist, load that site and scroll the window to that top
-  }
-
-  /**
-   * Generates the url to be shared for deep linking
-   * @param  {[type]} website     [description]
-   * @param  {[type]} topPosition [description]
-   */
-  function generateLink(website, topPosition) {
-    var url = 'www.linker.com/?website=' + window.encodeURIComponent(website) + '&top=' + topPosition;
-    $('.generated-link').val(url);
-  }
-
-  // function loadSite(website) {
-  //   if (website.indexOf('http://') === -1) {
-  //     website = 'http://' + website;
-  //   }
-
-  //   $.ajax({
-  //     type: 'post',
-  //     url: '/loadSite',
-  //     dataType: 'html',
-  //     data: {
-  //       url: website
-  //     },
-  //     success: function (data) {
-  //       console.log('Success')
-  //       $('.content').html(data);
-  //     }
-  //   });
+  'use strict';
 
   function loadSite(website) {
     window.location.href = '/loadSite?url=' + window.encodeURIComponent(formatWebsite(website));
@@ -52,12 +16,6 @@
 
     return website;
   }
-    // $('input').hide();
-    // $('iframe').prop('src', website).show();
-    // $('iframe').find('html').on('click', 'div', function () {
-    //   generateLink(website, $(this).css('top'));
-    // });
-  // }
 
   $('.enter-website').keypress(function(event) {
     if (event.which === 13) {
--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,19 +1,11 @@
 ;(function (window, $) {
   'use strict';
+
   /**
-   * Generates the url to be shared for deep linking
+   * Read a page's GET URL variables and return them as an associative array.
+   * @return {Object} Hash of query params to their values
+   * From stack overflow
    */
-  function generateLink(website, topPosition) {
-    var url = window.location.host + '/?url=' + window.encodeURIComponent(website) + '&top=' + topPosition,
-        $container = $('.generated-link'),
-        $input = $container.find('input');
-
-    $input.val(url);
-    $container.fadeIn();
-    $input.focus();
-  }
-
-  // Read a page's GET URL variables and return them as an associative array.
   function getUrlVars() {
     var vars = [], hash,
         hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
@@ -26,16 +18,34 @@
     return vars;
   }
 
+  /**
+   * Generates the url to be shared for deep linking
+   */
+  function generateLink(website, topPosition) {
+    var url = window.location.host + '/?url=' + window.encodeURIComponent(website) + '&top=' + topPosition,
+        $container = $('.generated-link'),
+        $input = $container.find('input');
+
+    $input.val(url);
+    $('.header').find('.instructions').hide();
+    $container.css('display', 'inline-block');
+    $input.focus();
+  }
+
   $('.linklink-content').on('click', '*', function (e) {
     var $this = $(this),
         top = $this.offset().top;
 
     // Stop the navigation of links
+    // Not sure why prevent default doesn't cut it
+    // for links
     $this.prop('href', 'javascript:void(0)');
 
     generateLink(getUrlVars()['url'], top);
     e.stopPropagation();
     return false;
   });
+
+  $('.header').slideDown('fast');
 
 })(window, window.jQuery);
